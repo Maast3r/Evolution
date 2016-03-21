4c5390c43b0a42f3e71e598a26eef1ee7cf88f22
Trevor Burch <burchtm@rose-hulman.edu>
2016-03-21 19:11:17 -0400

    Deck - testSize0 passes

---
+++ src/main/java/devops/hw1/core/Deck.java
@@ -0,0 +1,22 @@
+package devops.hw1.core;
+
+import java.util.Stack;
+
+/**
+ * Created by burchtm on 3/21/2016.
+ */
+public class Deck<T> extends Stack<T> {
+
+    public Deck(){
+        super();
+    }
+
+    /**
+     * Gets the amount of cards in the deck
+     * @return The number of cards in the deck
+     */
+    public int getSize(){
+        return 0;
+    }
+
+}
