1ea47e891814e5887a580d886a74b0be1dd6cdf5
Trevor Burch <burchtm@rose-hulman.edu>
2016-03-21 18:22:41 -0400

    Card - testDesc passes

--- src/main/java/devops/hw1/core/Card.java
+++ src/main/java/devops/hw1/core/Card.java
@@ -6,12 +6,26 @@ package devops.hw1.core;
 public class Card {
 
     String name;
+    String desc;
 
-    public Card(String name){
+    public Card(String name, String desc){
         this.name = name;
+        this.desc = desc;
     }
 
+    /**
+     * Returns the card's name
+     * @return The card's name
+     */
     public String getName(){
         return name;
+    }
+
+    /**
+     * Returns the card's description
+     * @return The card's description
+     */
+    public String getDesc() {
+        return desc;
     }
 }
