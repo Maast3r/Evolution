8e8a2547790becaec63ff81d03271d9f8497a214
Trevor Burch <burchtm@rose-hulman.edu>
2016-03-21 18:17:12 -0400

    Card - testDesc fails

--- src/test/java/devops/hw1/core/CardTests.java
+++ src/test/java/devops/hw1/core/CardTests.java
@@ -1,8 +1,8 @@
 package devops.hw1.core;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 /**
  * Created by burchtm on 3/21/2016.
@@ -11,7 +11,13 @@ public class CardTests {
 
     @Test
     public void testName(){
-        Card testCard = new Card("Carnivore");
+        Card testCard = new Card("Carnivore", "Makes a species a carnivore");
         assertTrue("Carnivore".equals(testCard.getName()));
+    }
+    
+    @Test
+    public void testDesc(){
+        Card testCard = new Card("Carnivore", "Makes a species a carnivore");
+        assertTrue("Makes a species a carnivore".equals(testCard.getDesc()));
     }
 }
