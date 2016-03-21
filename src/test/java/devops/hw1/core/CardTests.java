7a9b13a90c588180555b9fa1f5de562d7d7dde88
Trevor Burch <burchtm@rose-hulman.edu>
2016-03-21 18:34:20 -0400

    Card - testDirection fails

--- src/test/java/devops/hw1/core/CardTests.java
+++ src/test/java/devops/hw1/core/CardTests.java
@@ -13,25 +13,31 @@ public class CardTests {
 
     @Test
     public void testName(){
-        Card testCard = new Card("Carnivore", "Makes a species a carnivore", "./carnivore.jpg", 3);
+        Card testCard = new Card("Carnivore", "Makes a species a carnivore", "./carnivore.jpg", 3, 0);
         assertTrue("Carnivore".equals(testCard.getName()));
     }
 
     @Test
     public void testDesc(){
-        Card testCard = new Card("Carnivore", "Makes a species a carnivore", "./carnivore.jpg", 3);
+        Card testCard = new Card("Carnivore", "Makes a species a carnivore", "./carnivore.jpg", 3, 0);
         assertTrue("Makes a species a carnivore".equals(testCard.getDesc()));
     }
 
     @Test
     public void testImgPath(){
-        Card testCard = new Card("Carnivore", "Makes a species a carnivore", "./carnivore.jpg", 3);
+        Card testCard = new Card("Carnivore", "Makes a species a carnivore", "./carnivore.jpg", 3, 0);
         assertTrue("./carnivore.jpg".equals(testCard.getImgPath()));
     }
 
     @Test
     public void testFood(){
-        Card testCard = new Card("Carnivore", "Makes a species a carnivore", "./carnivore.jpg", 3);
+        Card testCard = new Card("Carnivore", "Makes a species a carnivore", "./carnivore.jpg", 3, 0);
         assertEquals(testCard.getFood(), 3);
+    }
+
+    @Test
+    public void testDirection(){
+        Card testCard = new Card("Carnivore", "Makes a species a carnivore", "./carnivore.jpg", 3, 0);
+        assertEquals(testCard.getDirection(), 0);
     }
 }
