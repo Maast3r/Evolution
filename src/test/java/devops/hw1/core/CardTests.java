7f2aa60bb2088e717757c800318bc5d681ad5868
Trevor Burch <burchtm@rose-hulman.edu>
2016-03-21 18:24:23 -0400

    Card - testImgPath fails

--- src/test/java/devops/hw1/core/CardTests.java
+++ src/test/java/devops/hw1/core/CardTests.java
@@ -11,13 +11,19 @@ public class CardTests {
 
     @Test
     public void testName(){
-        Card testCard = new Card("Carnivore", "Makes a species a carnivore");
+        Card testCard = new Card("Carnivore", "Makes a species a carnivore", "./carnivore.jpg");
         assertTrue("Carnivore".equals(testCard.getName()));
     }
-    
+
     @Test
     public void testDesc(){
-        Card testCard = new Card("Carnivore", "Makes a species a carnivore");
+        Card testCard = new Card("Carnivore", "Makes a species a carnivore", "./carnivore.jpg");
         assertTrue("Makes a species a carnivore".equals(testCard.getDesc()));
+    }
+
+    @Test
+    public void testImgPath(){
+        Card testCard = new Card("Carnivore", "Makes a species a carnivore", "./carnivore.jpg");
+        assertTrue("./carnivore.jpg".equals(testCard.getImgPath()));
     }
 }
