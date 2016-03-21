b04ed73c9ce94291b29a10ad4f79297e85d056d2
goistjt <goistjt@rose-hulman.edu>
2016-03-21 18:24:02 -0400

    Species testPopulation passes

--- src/main/java/devops/hw1/core/Species.java
+++ src/main/java/devops/hw1/core/Species.java
@@ -4,10 +4,12 @@ package devops.hw1.core;
  * Created by goistjt on 3/21/2016.
  */
 public class Species {
-    int bodySize;
+    private int bodySize;
+    private int population;
 
     public Species() {
         bodySize = 1;
+        population = 1;
     }
 
     /**
@@ -15,6 +17,14 @@ public class Species {
      * @return bodySize
      */
     public int getBodySize() {
+        return 1;
+    }
+
+    /**
+     * Returns the current population of the species
+     * @return population
+     */
+    public int getPopulation() {
         return 1;
     }
 }
