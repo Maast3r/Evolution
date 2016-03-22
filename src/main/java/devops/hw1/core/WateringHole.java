1c5aad4fb503d8e3af65afa99fd72d9f150717e1
Trevor Burch <burchtm@rose-hulman.edu>
2016-03-21 20:18:06 -0400

    WateringHole - testGetSize0 passes

---
+++ src/main/java/devops/hw1/core/WateringHole.java
@@ -0,0 +1,14 @@
+package devops.hw1.core;
+
+/**
+ * Manages the watering hole
+ * Created by burchtm on 3/21/2016.
+ */
+public class WateringHole {
+
+    int size = 0;
+
+    public int getSize() {
+        return size;
+    }
+}
