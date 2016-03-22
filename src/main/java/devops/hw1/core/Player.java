2e4358967f0022d05ff148153f0979f96d180a90
goistjt <goistjt@rose-hulman.edu>
2016-03-22 17:15:55 -0400

    Player testPlayerAddMultiSpecies passes

--- src/main/java/devops/hw1/core/Player.java
+++ src/main/java/devops/hw1/core/Player.java
@@ -6,12 +6,13 @@ import java.util.ArrayList;
  * Created by goistjt on 3/22/2016.
  */
 public class Player {
+    private ArrayList<ISpecies> speciesList = new ArrayList<>();
+
     public void addSpecies() {
+        this.speciesList.add(new TestSpecies());
     }
 
     public ArrayList<ISpecies> getSpecies() {
-        ArrayList<ISpecies> speciesList = new ArrayList<>();
-        speciesList.add(new TestSpecies());
-        return speciesList;
+        return this.speciesList;
     }
 }
