08ac11868ce34e45ff093416df5568108cd8e000
goistjt <goistjt@rose-hulman.edu>
2016-03-23 18:57:12 -0400

    Removed methods I had created that should be implemented via integration testing

--- src/main/java/devops/hw1/core/Game.java
+++ src/main/java/devops/hw1/core/Game.java
@@ -1,17 +1,10 @@
 package devops.hw1.core;
 
-import java.util.ArrayList;
-
 /**
  * Created by goistjt on 3/23/2016.
  */
 public class Game {
     private int round = 1;
-    private ArrayList<IPlayer> players;
-
-    public Game(ArrayList<IPlayer> players) {
-        this.players = players;
-    }
 
     public int getRound() {
         return this.round;
@@ -21,7 +14,4 @@ public class Game {
         this.round++;
     }
 
-    public ArrayList<IPlayer> getPlayers() {
-        return this.players;
-    }
 }
--- src/test/java/devops/hw1/core/GameTests.java
+++ src/test/java/devops/hw1/core/GameTests.java
@@ -2,9 +2,7 @@ package devops.hw1.core;
 
 import org.junit.Test;
 
-import java.util.ArrayList;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Created by goistjt on 3/23/2016.
@@ -12,43 +10,21 @@ import static org.junit.Assert.*;
 public class GameTests {
 
     @Test
-    public void testConstructWNumPlayers() {
-        ArrayList<IPlayer> players = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            players.add(new TestPlayer());
-        }
-        Game g = new Game(players);
-        assertEquals(4, g.getPlayers().size());
-    }
-
-    @Test
     public void testGetCurrentRound() {
-        ArrayList<IPlayer> players = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            players.add(new TestPlayer());
-        }
-        Game g = new Game(players);
+        Game g = new Game();
         assertEquals(1, g.getRound());
     }
 
     @Test
     public void testIncreaseRound() {
-        ArrayList<IPlayer> players = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            players.add(new TestPlayer());
-        }
-        Game g = new Game(players);
+        Game g = new Game();
         g.increaseRound();
         assertEquals(2, g.getRound());
     }
 
     @Test
     public void testIncreaseMultiRound() {
-        ArrayList<IPlayer> players = new ArrayList<>();
-        for (int i = 0; i < 4; i++) {
-            players.add(new TestPlayer());
-        }
-        Game g = new Game(players);
+        Game g = new Game();
         for (int i = 0; i < 4; i++) {
             g.increaseRound();
             assertEquals(i + 2, g.getRound());
