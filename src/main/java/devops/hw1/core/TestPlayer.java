29b95be60fbd2abf0f6baa3e445576eba2b550fb
goistjt <goistjt@rose-hulman.edu>
2016-03-23 18:54:31 -0400

    Refactored TestPlayer to construct with a single passed in ISpecies

--- src/main/java/devops/hw1/core/TestPlayer.java
+++ src/main/java/devops/hw1/core/TestPlayer.java
@@ -6,6 +6,13 @@ import java.util.ArrayList;
  * Created by goistjt on 3/22/2016.
  */
 public class TestPlayer implements IPlayer {
+    private ArrayList<ISpecies> species;
+
+    public TestPlayer(ISpecies species) {
+        this.species = new ArrayList<>();
+        this.species.add(species);
+    }
+
     @Override
     public void addSpeciesRight(ISpecies species) {
 
