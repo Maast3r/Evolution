b068ef13a2ac5cab79508979c20457fe7764ff70
goistjt <goistjt@rose-hulman.edu>
2016-03-23 18:23:55 -0400

    Game testIncreaseRound passes

--- src/main/java/devops/hw1/core/Game.java
+++ src/main/java/devops/hw1/core/Game.java
@@ -4,7 +4,13 @@ package devops.hw1.core;
  * Created by goistjt on 3/23/2016.
  */
 public class Game {
+    private int round = 1;
+
     public int getRound() {
-        return 1;
+        return this.round;
+    }
+
+    public void increaseRound() {
+        this.round = 2;
     }
 }
