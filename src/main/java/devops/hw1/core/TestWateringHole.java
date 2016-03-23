91672d1331e3c53e08d8effc7d332c3120bc6d51
goistjt <goistjt@rose-hulman.edu>
2016-03-23 17:59:35 -0400

    Added TestWateringHole for future Unit Testing

---
+++ src/main/java/devops/hw1/core/TestWateringHole.java
@@ -0,0 +1,31 @@
+package devops.hw1.core;
+
+/**
+ * Created by goistjt on 3/23/2016.
+ */
+public class TestWateringHole implements IWateringHole {
+    @Override
+    public int getFoodCount() {
+        return 0;
+    }
+
+    @Override
+    public void addFood() {
+
+    }
+
+    @Override
+    public void addFood(int i) {
+
+    }
+
+    @Override
+    public void removeFood() {
+
+    }
+
+    @Override
+    public void removeFood(int i) {
+
+    }
+}
