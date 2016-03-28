1aff2d4144fb44c6791619d172407b823e7e3e5c
Andrew Ma <maastergotmail@gmail.com>
2016-03-28 17:33:51 -0400

    testinitrandom fails

--- src/test/java/com/Evolution/PhaseOneTests.java
+++ src/test/java/com/Evolution/PhaseOneTests.java
@@ -1,10 +1,12 @@
 package com.Evolution;
 
+import com.Evolution.exceptions.IllegalCardDirectionException;
 import com.Evolution.interfaces.IPlayer;
 import com.Evolution.logic.*;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Random;
 
 import static org.junit.Assert.assertEquals;
 
@@ -23,5 +25,27 @@ public class PhaseOneTests {
         assertEquals(1, p.getPlayers().size());
         assertEquals(0, p.getDrawPile().size());
         assertEquals(0, p.getDiscardPile().size());
+    }
+
+    @Test
+    public void testInitRandom() throws IllegalCardDirectionException {
+        ArrayList<IPlayer> players = new ArrayList<IPlayer>();
+        players.add(new Player(new Species()));
+        Random rn = new Random();
+        int random = rn.nextInt(1000 - 0 + 1) + 0;
+        Deck<Card> drawPile = new Deck();
+        Deck<Card> discardPile = new Deck();
+        for(int i=0; i<random; i++){
+            drawPile.add(new Card("Carnivore", "Makes a species a carnivore",
+                    "./carnivore.jpg", 3, 0));
+            discardPile.add(new Card("Carnivore", "Makes a species a carnivore",
+                    "./carnivore.jpg", 3, 0));
+        }
+
+
+        PhaseOne p = new PhaseOne(players, drawPile, discardPile);
+        assertEquals(1, p.getPlayers().size());
+        assertEquals(random, p.getDrawPile().size());
+        assertEquals(random, p.getDiscardPile().size());
     }
 }
