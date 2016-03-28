4b8593c5d49ceab2947df3d8ab3af9610b8e8ceb
goistjt <goistjt@rose-hulman.edu>
2016-03-28 19:09:07 -0400

    DeckFactory testReadLineToCard2 passes

--- src/main/java/com/Evolution/logic/DeckFactory.java
+++ src/main/java/com/Evolution/logic/DeckFactory.java
@@ -4,6 +4,7 @@ import com.Evolution.exceptions.IllegalCardDirectionException;
 import com.Evolution.interfaces.ICard;
 
 import java.io.ByteArrayInputStream;
+import java.nio.charset.StandardCharsets;
 
 /**
  * Created by goistjt on 3/28/2016.
@@ -11,6 +12,12 @@ import java.io.ByteArrayInputStream;
 public class DeckFactory {
 
     public ICard readLineToCard(ByteArrayInputStream input) throws IllegalCardDirectionException {
-        return new Card("Carnivore", "Makes a species a carnivore", "./carnivore.jpg", 3, 0);
+        int n = input.available();
+        byte[] bytes = new byte[n];
+        input.read(bytes, 0, n);
+        String s = new String(bytes, StandardCharsets.UTF_8);
+        String[] cardParams = s.split(";");
+        return new Card(cardParams[0], cardParams[1], cardParams[2], Integer.parseInt(cardParams[3]),
+                Integer.parseInt(cardParams[4]));
     }
 }
