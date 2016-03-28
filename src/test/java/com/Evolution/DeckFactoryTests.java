e52f53fe7bad806dd70bce884524d0a0a407c4af
goistjt <goistjt@rose-hulman.edu>
2016-03-28 19:02:09 -0400

    DeckFactory testReadLineToCard2 fails

--- src/test/java/com/Evolution/DeckFactoryTests.java
+++ src/test/java/com/Evolution/DeckFactoryTests.java
@@ -25,7 +25,18 @@ public class DeckFactoryTests {
         assertTrue(c.getImgPath().equals("./carnivore.jpg"));
         assertTrue(c.getFood() == 3);
         assertTrue(c.getDirection() == 0);
+    }
 
+    @Test
+    public void testReadLineToCard2() throws IllegalCardDirectionException {
+        DeckFactory df = new DeckFactory();
+        ICard c = df.readLineToCard(
+                new ByteArrayInputStream("Random;Does random stuff;./random.jpg;2;1".getBytes()));
+        assertTrue(c.getName().equals("Random"));
+        assertTrue(c.getDesc().equals("Does random stuff"));
+        assertTrue(c.getImgPath().equals("./random.jpg"));
+        assertTrue(c.getFood() == 2);
+        assertTrue(c.getDirection() == 1);
     }
 
 }
