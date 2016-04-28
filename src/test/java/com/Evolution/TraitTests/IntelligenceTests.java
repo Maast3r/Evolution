1b7c6d69ce18ab7021a1a0394d7e1849e7d74244
Andrew Ma <maastergotmail@gmail.com>
2016-04-27 23:18:30 -0400

    testGetGame passes

--- src/test/java/com/Evolution/TraitTests/IntelligenceTests.java
+++ src/test/java/com/Evolution/TraitTests/IntelligenceTests.java
@@ -4,6 +4,7 @@ package com.Evolution.TraitTests;
 import com.Evolution.interfaces.ATrait;
 import com.Evolution.logic.Game;
 import com.Evolution.traits.Foraging;
+import com.Evolution.traits.Intelligence;
 import org.easymock.EasyMock;
 import org.junit.Test;
 
