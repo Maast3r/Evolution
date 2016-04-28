743a9d11b7a33614d3da404b1c6dcec4d1fc2dc9
Andrew Ma <maastergotmail@gmail.com>
2016-04-27 23:16:25 -0400

    testGetGame fails

---
+++ src/test/java/com/Evolution/TraitTests/IntelligenceTests.java
@@ -0,0 +1,19 @@
+package com.Evolution.TraitTests;
+
+
+import com.Evolution.interfaces.ATrait;
+import com.Evolution.logic.Game;
+import com.Evolution.traits.Foraging;
+import org.easymock.EasyMock;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class IntelligenceTests {
+    @Test
+    public void testGetGame() {
+        Game g = EasyMock.niceMock(Game.class);
+        ATrait t = new Intelligence(g);
+        assertEquals(g, t.getGame());
+    }
+}
