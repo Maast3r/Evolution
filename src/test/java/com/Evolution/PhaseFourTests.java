eac1607cb110604c96d30efa6e36cfb8c905b972
goistjt <goistjt@rose-hulman.edu>
2016-04-27 21:28:44 -0400

    testNextPhase fails

--- src/test/java/com/Evolution/PhaseFourTests.java
+++ src/test/java/com/Evolution/PhaseFourTests.java
@@ -1,8 +1,12 @@
 package com.Evolution;
 
+import com.Evolution.exceptions.*;
+import com.Evolution.interfaces.*;
 import com.Evolution.logic.*;
 import org.easymock.EasyMock;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
@@ -23,5 +27,26 @@ public class PhaseFourTests {
         Game g = EasyMock.niceMock(Game.class);
         PhaseFour p = new PhaseFour(g);
         assertEquals(p.getNumber(), 4);
+    }
+
+    @Test
+    public void testNextPhase() throws IllegalCardDirectionException, IllegalNumberOfPlayers, DeckEmptyException, InvalidPlayerSelectException, NullGameObjectException {
+        ArrayList<IPlayer> players = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            players.add(EasyMock.niceMock(Player.class));
+        }
+        Game g = new Game(players, EasyMock.niceMock(WateringHole.class), EasyMock.niceMock(Deck.class),
+                EasyMock.niceMock(Deck.class));
+        PhaseFour p = new PhaseFour(g);
+        g.setPhase(p);
+        for (IPlayer player :
+                players) {
+            EasyMock.expect(player.allSpeciesFull()).andReturn(true);
+        }
+
+        players.forEach(EasyMock::replay);
+        p.execute();
+        assertEquals(PhaseOne.class, g.getPhase().getClass());
+        players.forEach(EasyMock::verify);
     }
 }
