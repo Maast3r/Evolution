11cdc0ddb5322b0e0e5943b3baa0e3ae7756f687
Andrew Ma <maastergotmail@gmail.com>
2016-04-28 14:43:10 -0400

    testIncreasePop  fails

--- src/test/java/com/Evolution/TraitTests/FertileTests.java
+++ src/test/java/com/Evolution/TraitTests/FertileTests.java
@@ -1,7 +1,10 @@
 package com.Evolution.TraitTests;
 
 
+import com.Evolution.exceptions.*;
 import com.Evolution.interfaces.ATrait;
+import com.Evolution.interfaces.ICard;
+import com.Evolution.logic.Card;
 import com.Evolution.logic.Game;
 import com.Evolution.traits.Fertile;
 import org.easymock.EasyMock;
@@ -10,10 +13,26 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class FertileTests {
+
     @Test
     public void testGetGame() {
         Game g = EasyMock.niceMock(Game.class);
         ATrait t = new Fertile(g);
         assertEquals(g, t.getGame());
+    }
+
+    @Test
+    public void testIncreasePop() throws
+            IllegalCardRemovalException, NullGameObjectException,
+            SpeciesPopulationException, IllegalCardDiscardException,
+            InvalidPlayerSelectException, IllegalSpeciesIndexException,
+            IllegalCardFoodException, WateringHoleEmptyException,
+            SpeciesFullException, IllegalCardDirectionException {
+        Game g = EasyMock.niceMock(Game.class);
+        ATrait t = new Fertile(g);
+        g.increasePopulation(0, 0);
+        EasyMock.replay(g);
+        t.executeTrait(new int[]{0, 0}, new int[]{0, 0});
+        EasyMock.verify(g);
     }
 }
