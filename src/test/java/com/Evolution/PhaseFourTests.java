705e1a09188e7a3491d7e27d78d1e60e97703e38
goistjt <goistjt@rose-hulman.edu>
2016-04-27 22:27:08 -0400

    testNextRound fails

--- src/test/java/com/Evolution/PhaseFourTests.java
+++ src/test/java/com/Evolution/PhaseFourTests.java
@@ -31,10 +31,7 @@ public class PhaseFourTests {
 
     @Test
     public void testNextPhase() throws IllegalCardDirectionException, IllegalNumberOfPlayers, DeckEmptyException, InvalidPlayerSelectException, NullGameObjectException {
-        ArrayList<IPlayer> players = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            players.add(EasyMock.niceMock(Player.class));
-        }
+        ArrayList<IPlayer> players = generateMockedPlayers(3);
         Game g = new Game(players, EasyMock.niceMock(WateringHole.class), EasyMock.niceMock(Deck.class),
                 EasyMock.niceMock(Deck.class));
         PhaseFour p = new PhaseFour(g);
@@ -52,10 +49,7 @@ public class PhaseFourTests {
 
     @Test
     public void testNextTurn() throws IllegalNumberOfPlayers, NullGameObjectException, InvalidPlayerSelectException, IllegalCardDirectionException, DeckEmptyException {
-        ArrayList<IPlayer> players = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            players.add(EasyMock.niceMock(Player.class));
-        }
+        ArrayList<IPlayer> players = generateMockedPlayers(3);
         Game g = new Game(players, EasyMock.niceMock(WateringHole.class), EasyMock.niceMock(Deck.class),
                 EasyMock.niceMock(Deck.class));
         PhaseFour p = new PhaseFour(g);
@@ -65,7 +59,35 @@ public class PhaseFourTests {
         players.forEach(EasyMock::replay);
 
         p.execute();
-        assertEquals(initTurn+1, g.getTurn());
+        assertEquals(initTurn + 1, g.getTurn());
         players.forEach(EasyMock::verify);
+    }
+
+    @Test
+    public void testNextRound() throws IllegalCardDirectionException, IllegalNumberOfPlayers, DeckEmptyException,
+            InvalidPlayerSelectException, NullGameObjectException {
+        ArrayList<IPlayer> players = generateMockedPlayers(3);
+        Game g = new Game(players, EasyMock.niceMock(WateringHole.class), EasyMock.niceMock(Deck.class),
+                EasyMock.niceMock(Deck.class));
+        PhaseFour p = new PhaseFour(g);
+        g.setPhase(p);
+        for (IPlayer player :
+                players) {
+            EasyMock.expect(player.allSpeciesFull()).andReturn(true);
+        }
+
+        int curRound = g.getRound();
+        players.forEach(EasyMock::replay);
+        p.execute();
+        assertEquals(curRound + 1, g.getRound());
+        players.forEach(EasyMock::verify);
+    }
+
+    private ArrayList<IPlayer> generateMockedPlayers(int numPlayers) {
+        ArrayList<IPlayer> players = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            players.add(EasyMock.niceMock(Player.class));
+        }
+        return players;
     }
 }
