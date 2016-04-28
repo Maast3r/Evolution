098119ac9924e65e3d19ee85e5b77771572e4d9d
Andrew Ma <maastergotmail@gmail.com>
2016-04-27 23:14:30 -0400

    testCallGameFeedRight2  passes

--- src/test/java/com/Evolution/TraitTests/CooperationTests.java
+++ src/test/java/com/Evolution/TraitTests/CooperationTests.java
@@ -33,4 +33,15 @@ public class CooperationTests {
         t.executeTrait(new int[]{0, 0}, new int[]{1, 0});
         EasyMock.verify(g);
     }
+
+    @Test
+    public void testCallGameFeedRight2() throws IllegalSpeciesIndexException, InvalidPlayerSelectException,
+            SpeciesFullException, WateringHoleEmptyException {
+        Game g = EasyMock.niceMock(Game.class);
+        ATrait t = new Cooperation(g);
+        g.feedPlayerSpecies(0, 2);
+        EasyMock.replay(g);
+        t.executeTrait(new int[]{0, 0}, new int[]{2, 0});
+        EasyMock.verify(g);
+    }
 }
