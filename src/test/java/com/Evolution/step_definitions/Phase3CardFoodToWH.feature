e08817e25b1e3df446ae31328e82524e01ed0485
goistjt <goistjt@rose-hulman.edu>
2016-05-02 19:03:00 -0400

    I think I have cucumber working for intelliJ

---
+++ src/test/java/com/Evolution/step_definitions/Phase3CardFoodToWH.feature
@@ -0,0 +1,12 @@
+# Created by goistjt at 5/2/2016
+Feature: Phase3CardFoodToWH
+
+
+  Scenario: CardFoodToWH
+    Given I have a new Game
+    And it is currently Phase3
+    And I have an empty Watering Hole
+    And there are Cards on the Watering Hole
+    When Phase3 ends
+    Then the food on the Cards shall be added to the Watering Hole
+    And the Cards shall be discarded
---
+++ src/test/java/com/Evolution/steps/MyStepdefs.java
@@ -0,0 +1,54 @@
+package com.Evolution.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by goistjt on 5/2/2016.
+ */
+public class MyStepdefs {
+    @Given("^I have a new Game$")
+    public void iHaveANewGame() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^it is currently Phase(\\d+)$")
+    public void itIsCurrentlyPhase(int arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^I have an empty Watering Hole$")
+    public void iHaveAnEmptyWateringHole() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^there are Cards on the Watering Hole$")
+    public void thereAreCardsOnTheWateringHole() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Phase(\\d+) ends$")
+    public void phaseEnds(int arg0) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the food on the Cards shall be added to the Watering Hole$")
+    public void theFoodOnTheCardsShallBeAddedToTheWateringHole() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^the Cards shall be discarded$")
+    public void theCardsShallBeDiscarded() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
