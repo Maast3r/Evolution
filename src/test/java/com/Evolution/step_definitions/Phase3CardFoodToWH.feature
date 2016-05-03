e7c30656f68254e6a241fc623e50ccf36310901e
goistjt <goistjt@rose-hulman.edu>
2016-05-03 15:17:55 -0400

    FirstPlayerUpdate feature defined

--- src/test/java/com/Evolution/step_definitions/Phase3CardFoodToWH.feature
+++ src/test/java/com/Evolution/step_definitions/Phase3CardFoodToWH.feature
@@ -2,11 +2,16 @@
 Feature: Phase3CardFoodToWH
 
 
-  Scenario: CardFoodToWH
-    Given I have a new Game with 4 players
+  Scenario Outline: CardFoodToWH
+    Given I have a new Game with <Num Players> players
     And it is currently Phase3
     And I have an empty Watering Hole
     And there are Cards on the Watering Hole
     When Phase3 ends
     Then the food on the Cards shall be added to the Watering Hole
     And the Cards shall be discarded
+    Examples:
+      |Num Players  |
+      |3            |
+      |4            |
+      |5            |
---
+++ src/test/java/com/Evolution/steps/FirstPlayerUpdate.java
@@ -0,0 +1,47 @@
+package com.Evolution.steps;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+/**
+ * Created by goistjt on 5/3/2016.
+ */
+public class FirstPlayerUpdate {
+    @And("^it is currently Round (\\d+)$")
+    public void itIsCurrentlyRoundRound(int round) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^the First Player is currently Player (\\d+)$")
+    public void theFirstPlayerIsCurrentlyPlayerFirst_player_init(int initPlayer) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^it is currently Phase4$")
+    public void itIsCurrentlyPhase() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @When("^Phase4 ends$")
+    public void phaseEnds() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @Then("^the Round shall increment$")
+    public void theRoundShallIncrement() throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+
+    @And("^the First Player shall be Player (\\d+)$")
+    public void theFirstPlayerShallBePlayerFirst_player_result(int resultPlayuer) throws Throwable {
+        // Write code here that turns the phrase above into concrete actions
+        throw new PendingException();
+    }
+}
